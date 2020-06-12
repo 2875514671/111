0e2cd799118b3e467691de223f7f883a97230d44
EmperorHons <ysk19950809@163.com>
2020-06-12 23:44:40 +0800

    修改 Wallpaper.py 下载壁纸失效

--- Wallpaper/Wallpaper.py
+++ Wallpaper/Wallpaper.py
@@ -24,11 +24,22 @@ def save_to_document(urls):
     name = cop.sub('', n)
     print(name)
     # 文件保存地址
-    root = r"E:\Jetbrains\PycharmProjects\Reptile\Wallpaper\Wallpaper"
+    root = r"D:\JetBrains\PycharmProjects\Reptile\Wallpaper\Wallpaper"
 
     img_name = name + '.jpg'
     if not os.path.exists(img_name):
-        img = requests.get(imgs)
+        """
+        2020-06-12 添加 headers 解决图片下载失败
+        根据请求图片地址发现抱错{"code":"40310014","msg":"invalid Referer header"}，
+        参照大佬的描述 https://www.jianshu.com/p/46c127a699f7
+        请求头中添加 Referer，解决此问题
+        """
+        # 2020-06-12 根据请求图片地址发现抱错{"code":"40310014","msg":"invalid Referer header"}，请求头中添加 Referer
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/83.0.4103.97 Safari/537.36 ",
+                   "Referer": "https://wallpaper.wispx.cn/"
+                   }
+        img = requests.get(imgs, headers=headers)
         with open(root + '/' + img_name, 'wb') as f:
             time.sleep(0.5)
             f.write(img.content)
@@ -44,5 +55,5 @@ def main():
 
 
 if __name__ == '__main__':
-    for i in range(5):
+    for i in range(6):
         main()
